736bd1932b0b54fd0b350f6c9be1cb6784f17b31
Fausto Busso <faustobusso@gmail.com>
2024-12-14 03:02:23 -0300

    Actualizaciones
    Movimiento del personaje mejorado, salto y creación de arma

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+}
--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,34 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-        public float playerSpeed = 2.0f;
-        Rigidbody rb;
+    public CharacterController charCon;
+    public Transform camTrans;
 
-        void Start()
-        {   
-            rb = GetComponent<Rigidbody>();
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
+    [Header("Gravity")]
+    public float gravedadModificar;
 
-        void Update()
+    [Header("Move controls")]
+    public float moveSpeed;
+    public float jumpPower;
+    private bool canJump;
+    public Transform groundCheckPoint;
+    public LayerMask isGround; 
+
+    [Header("Camera controls")]
+    public float mouseSensibilidad;
+    public bool invertX;
+    public bool invertY;
+
+    private Vector3 moveInput;
+
+    private void Update()
+    {
+        //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        //Guardar Y velocity
+        float yStore = moveInput.y;
+
+        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horiMove = transform.right * Input.GetAxisRaw("Horizontal");
+
+        moveInput = horiMove + vertMove;
+        moveInput.Normalize();
+        moveInput = moveInput * moveSpeed;
+
+        moveInput.y = yStore;
+
+        //Gravedad
+        moveInput.y += Physics.gravity.y * gravedadModificar * Time.deltaTime;
+        
+        if (charCon.isGrounded)
         {
-            // Obtener la entrada de movimiento (Horizontal y Vertical)
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-            // Convertir el movimiento en relación con la rotación del jugador (espacio local)
-            Vector3 moveRelative = transform.TransformDirection(move) * playerSpeed * Time.deltaTime;
-
-            // Mover el jugador usando MovePosition para evitar acumulación de fuerzas
-            rb.MovePosition(rb.position + moveRelative);
-
-            // Desbloquear el cursor cuando se presiona Escape
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                Cursor.lockState = CursorLockMode.None;
-            }
+            moveInput.y = Physics.gravity.y * gravedadModificar * Time.deltaTime;
         }
+        
+        canJump = Physics.OverlapSphere(groundCheckPoint.position, .25f, isGround).Length > 0;
+        
+        //Salto del jugador
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            moveInput.y = jumpPower;    
+        }
+
+        charCon.Move(moveInput * Time.deltaTime);
+
+        //Control rotación cámara
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensibilidad;
+        
+        if (invertX)
+        {
+            mouseInput.x = -mouseInput.x;
+        }
+        if (invertY)
+        {
+            mouseInput.y = -mouseInput.y;
+        }
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+    }
 }
+
+
