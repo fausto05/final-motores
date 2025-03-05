87e564bce591a1bbaf79eca07d3b03b94b94c7d6
Fausto Busso <faustobusso@gmail.com>
2025-03-05 19:10:06 -0300

    Actualizaciones
    oleadas terminadas y decoracion de la escena principal

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public int defeatedEnemies;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-
         defeatedEnemies = 0;
+    }
+
+    public void WinGame()
+    {
+        Debug.Log("¡Has ganado el juego!");
+        // Aquí puedes agregar lógica para mostrar una pantalla de victoria
     }
 }
--- Assets/scripts/SpawnManager.cs
+++ Assets/scripts/SpawnManager.cs
@@ -6,65 +6,81 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] spawnPoints;
     public GameObject[] enemies;
-    public bool spawning;
-    private int waveCount;
+    private bool spawning;
     private int wave;
-    private int enemyType;
+    private int enemiesToSpawn;
     private int enemiesSpawned;
     private GameManager gameManager;
+    private int enemyType;
 
     private void Start()
     {
         spawning = false;
+        wave = 1; // Primera oleada
+        enemiesToSpawn = 2; // Inicia con 2 enemigos
         enemiesSpawned = 0;
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     private void Update()
     {
-        if (spawning == false && enemiesSpawned == gameManager.defeatedEnemies)
+        // Espera a que todos los enemigos sean derrotados antes de iniciar la siguiente oleada
+        if (!spawning && gameManager.defeatedEnemies == enemiesSpawned)
         {
-            StartCoroutine(SpawnWave(waveCount));
+            // Si ya terminaste la tercera oleada, ganar el juego
+            if (wave > 3)
+            {
+                gameManager.WinGame();
+                return;
+            }
+
+            StartCoroutine(SpawnWave());
         }
     }
 
-    IEnumerator SpawnWave(int waveC)
+    IEnumerator SpawnWave()
     {
         spawning = true;
-        yield return new WaitForSeconds(4);
-
-        for (int i = 0; i < waveC; i++)
-        {
-            SpawnEnemy(wave);
-            yield return new WaitForSeconds(2);
-        }
-        wave += 1;
-        waveCount += 2;
-        spawning = false;
+        gameManager.defeatedEnemies = 0; // Reiniciar contador de enemigos derrotados
+        yield return new WaitForSeconds(4); // Espera antes de iniciar la oleada
 
         enemiesSpawned = 0;
 
-        yield break;
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(2); // Espera entre spawns
+        }
+
+        // Aumentar oleada pero no permitir que pase de 3
+        wave++;
+        if (wave <= 3)
+        {
+            enemiesToSpawn *= 2; // Duplica la cantidad de enemigos por oleada
+        }
+
+        spawning = false;
     }
 
-    void SpawnEnemy(int wave)
+    void SpawnEnemy()
     {
-        int spawnPos = Random.Range(0, spawnPoints.Length); // Corregido
+        int spawnPos = Random.Range(0, spawnPoints.Length);
 
+        // Selección del tipo de enemigo según la oleada
         if (wave == 1)
         {
-            enemyType = 0; // Se selecciona el primer tipo de enemigo
+            enemyType = 0;
         }
-        else if (wave < 4)
+        else if (wave < 3)
         {
-            enemyType = Random.Range(0, Mathf.Min(enemies.Length, 2)); // Corregido
+            enemyType = Random.Range(0, Mathf.Min(enemies.Length, 2));
         }
         else
         {
-            enemyType = Random.Range(0, enemies.Length); // Corregido
+            enemyType = Random.Range(0, enemies.Length);
         }
 
         Instantiate(enemies[enemyType], spawnPoints[spawnPos].transform.position, spawnPoints[spawnPos].transform.rotation);
-        enemiesSpawned += 1;
+        enemiesSpawned++;
     }
 }
